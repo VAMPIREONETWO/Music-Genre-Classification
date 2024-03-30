6ea9995c882d463ea0bfa403e8f8d4b9a7f72906
Morgan <141666207+Morgan3450@users.noreply.github.com>
2024-03-30 11:31:55 +0000

    Update FCN.py

--- models/FCN.py
+++ models/FCN.py
@@ -1,80 +1,57 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-class FCN7(nn.Module):
-    def __init__(self, in_channels, num_classes):
-        super(FCN7, self).__init__()
-
-        # Preprocessing layer
-        self.pl = nn.Sequential(
-            nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        )
-
-        # Residual Blocks
-        self.block1 = ResidualBlock(64, 128)
-        self.block2 = ResidualBlock(128, 256)
-        self.block3 = ResidualBlock(256, 512, 2)
-        self.block4 = ResidualBlock(512, 1024, 2, dropout=True)
-        self.block5 = ResidualBlock(1024, 2048, 2)
-        self.block6 = ResidualBlock(2048, 4096, 2)
-        self.block7 = ResidualBlock(4096, num_classes, 1)  # Output channels = num_classes
+class ConvBlock(nn.Module):
+    def __init__(self, in_filters, out_filters, stride=1):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2d(in_filters, out_filters, kernel_size=3, stride=stride, padding=1)
+        self.bn = nn.BatchNorm2d(out_filters)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.pl(x)
-        x = self.block1(x)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
-        x = self.block5(x)
-        x = self.block6(x)
-        x = self.block7(x)
-
-        # Global Average Pooling and classification
-        x = F.adaptive_avg_pool2d(x, (1, 1))
-        x = torch.flatten(x, 1)
-
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
         return x
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, dropout=False):
-        super(ResidualBlock, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels)
-            )
-
-        self.dropout = nn.Dropout(0.5) if dropout else None
+class FCN7(nn.Module):
+    def __init__(self):
+        super(FCN7, self).__init__()
+        self.conv1 = ConvBlock(1, 128)
+        self.mp1 = nn.MaxPool2d((2, 4))
+        
+        self.conv2 = ConvBlock(128, 256)
+        self.mp2 = nn.MaxPool2d((2, 4))
+        
+        self.conv3 = ConvBlock(256, 512)
+        self.mp3 = nn.MaxPool2d((2, 4))
+        
+        self.conv4 = ConvBlock(512, 1024)
+        self.mp4 = nn.MaxPool2d((3, 5))
+        
+        self.conv5 = ConvBlock(1024, 2048)
+        self.mp5 = nn.MaxPool2d((4, 4))
+        
+        self.conv6 = nn.Conv2d(2048, 1024, kernel_size=1) # 1x1 convolutions
+        self.conv7 = nn.Conv2d(1024, 1024, kernel_size=1) # additional 1x1 convolution as per FCN-7
+        
+        self.final_conv = nn.Conv2d(1024, 50, kernel_size=1)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        identity = self.shortcut(x)
+        x = self.mp1(self.conv1(x))
+        x = self.mp2(self.conv2(x))
+        x = self.mp3(self.conv3(x))
+        x = self.mp4(self.conv4(x))
+        x = self.mp5(self.conv5(x))
+        
+        x = self.conv6(x)
+        x = self.conv7(x)
+        
+        x = self.final_conv(x)
+        x = x.view(x.size(0), -1)  # Flatten the output
+        x = self.sigmoid(x)
+        return x
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.dropout:
-            out = self.dropout(out)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-# model = FCN7(in_channels=1, num_classes=50)
+# fcn7 = FCN7()
+# output = fcn7(inputs)
